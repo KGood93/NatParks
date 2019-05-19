f41a2f0e16f223757a82bb9cfccc636424527d55
KA Goodreau <goodreau.kath@gmail.com>
2019-05-19 08:57:36 -0500

    featuresAdded

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 'use strict';
 
-let searchUser = "";
+let searchParks;
+let maxResults;
+const apiKey = '2YXWmohbDjS9FjhU6XQjt3TbqZS33VdyX4haBh2s';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
-function getUser() {
-    let url = "https://api.github.com/users/" + searchUser + "/repos";
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function getParks(searchParks, maxResults) {
+
+    const params = {
+        stateCode: searchParks,
+        limit: maxResults,
+        start: 0,
+        api_key: apiKey
+    };
+
+    let queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
     console.log(url);
 
     fetch(url)
-        .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
-        .catch(error => alert('Something went wrong. Try again later.'));
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson))
+      .catch(error => console.log('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
-    let final = $('<ul id="results-list"></ul>');
+    console.log(responseJson);
+    console.log(responseJson.data.length);
+    let parks = $('<ul id="results-list"></ul>');
 
-    for(let i = 0; i < responseJson.length; i++) {
-      final.append(`<li>${responseJson[i].name} - <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a></li>`);
+    for (let i=0; i <= responseJson.data.length - 1; i++) {
+      parks.append(`<li><h3>${responseJson.data[i].fullName}</h3><p>${responseJson.data[i].description}</p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></li>`);
+      console.log(responseJson.data[i].name);
     }
-
-    $('#results').replaceWith(final);
+    //display the results section
+    console.log(parks);
+    $('#results-list').replaceWith(parks);
+   
+    $('#results').removeClass('hidden');
+  
   }
+  
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    searchUser = $('#js-search-user').val();
-    console.log(searchUser);
-    getUser();
+    searchParks = $('#js-search-parks').val();
+    maxResults = $('#js-max-results').val();
+    console.log(searchParks);
+    getParks(searchParks, maxResults);
   });
 }
 
